8109cc653791ee5dbd713448c24c7d99397b1b0e
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-26 23:48:49 +0100

    Add "viewDidAppear" Swift snippet

---
+++ Swift/vda.swift
@@ -0,0 +1,10 @@
+---
+title: "viewDidAppear"
+summary: "Overriden viewDidAppear"
+completion-scope: Class Implementation
+---
+
+override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    <#implementation#>
+}
